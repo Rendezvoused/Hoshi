ede9d0b0359772bf4ede40cb61bfbb1f3ca05558
Rendezvoused <89423721+Rendezvoused@users.noreply.github.com>
2022-10-22 23:35:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+@font-face { font-family: "hamburg hand"; src: url('https://dl.dropboxusercontent.com/s/56z40obcsbbx6b3/hamburghandmint.otf') ;} 
+    @font-face { font-family: "bodini italic"; src: url('https://dl.dropboxusercontent.com/s/suxwvhmcw7qrtkt/bodinimint.otf') ;} 
+    @font-face { font-family: "bodini regular"; src: url('https://dl.dropboxusercontent.com/s/7r8a4dcat9y8gv9/bodinimintreg.otf') ;} 
+    @font-face { font-family: "migha"; src: url('https://dl.dropboxusercontent.com/s/n9bdttblpkv0o3v/mighamint.otf') ;} 
+    @font-face { font-family: "tuesday night"; src: url('https://dl.dropboxusercontent.com/s/k4vujfq88hoi72z/tuesdaynightmint.otf') ;}
+    @font-face { font-family: "signatura"; src: url('https://dl.dropboxusercontent.com/s/nl9aw30dw7zdt0f/signaturamonolinemint.ttf') ;}
+    @font-face { font-family: "hunter river"; src: url('https://dl.dropboxusercontent.com/s/6dlyze9durc8jdz/hunterrivermint.otf') ;}
+ @font-face { font-family: "got"; src: url('https://www.dropbox.com/s/k2f0nirevhc9v70/Game%20of%20Thrones.ttf') ;}
